cff89cbd3547ed5cd8d5611104d71e923b857511
Yukisan-uwo <161091611+Yukisan-uwo@users.noreply.github.com>
2025-11-25 15:15:45 -0500

    a

--- BYTEAZULPROFESIONAL/fmGestionarMedicina.cs
+++ BYTEAZULPROFESIONAL/fmGestionarMedicina.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaLogica;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace BYTEAZULPROFESIONAL
 {
     public partial class fmGestionarMedicina : Form
     {
+        CsMedicinas csmedicina;
         public fmGestionarMedicina()
         {
             InitializeComponent();
@@ -40,6 +42,18 @@ namespace BYTEAZULPROFESIONAL
             {
                 MessageBox.Show("Error al seleccionar el producto: " + ex.Message);
             }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            csmedicina = new CsMedicinas();
+            dgvVerMedicina.DataSource = csmedicina.Buscar(txtBuscar.Text.Trim());
+        }
+
+        private void fmGestionarMedicina_Load(object sender, EventArgs e)
+        {
+            csmedicina = new CsMedicinas();
+            dgvVerMedicina.DataSource = csmedicina.Buscar(txtBuscar.Text.Trim());
         }
     }
 }
---
+++ CapaDatos/BdMedicinas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class BdMedicinas
+    {
+        BdConexionSQL BdConexion;
+        public DataTable BuscarMedicina(string filtro)
+        {
+            DataTable dtVerMedicina = new DataTable();
+            try
+            {
+                BdConexion = new BdConexionSQL();
+                using (SqlConnection conn = BdConexion.ObtenerConexion())
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("SP_SL_TbProductos_Listado", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@NombreProducto", filtro);
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        da.Fill(dtVerMedicina);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener lista de medicina: " + ex.Message);
+            }
+            return dtVerMedicina;
+        }
+        //---------------------------------------------------------------
+    }
+}
